b8567dcef8d217c062c7fcf6ea9373f4c814287c
pBinarySoul <pBinarySoul@gmail.com>
2019-07-17 10:14:50 +0300

    Компонент корзины

---
+++ components/Cart/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Cart = (props) =>
+{
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Cart;
