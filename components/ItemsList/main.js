e034cc3ef0968754b17b27c3255b60a90fb7cf4a
pBinarySoul <pBinarySoul@gmail.com>
2019-07-17 10:15:18 +0300

    Компонент списка товаров

---
+++ components/ItemsList/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const ItemsList = (props) =>
+{
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default ItemsList;
