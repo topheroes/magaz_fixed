00c1aa55ffa32b0260937f9b691b1edcfca63e60
pBinarySoul <pBinarySoul@gmail.com>
2019-07-17 10:15:54 +0300

    Компонент с заказами

---
+++ components/Orders/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Orders = (props) =>
+{
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Orders;
